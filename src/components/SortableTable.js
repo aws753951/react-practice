d3abb4adc65a839bedcf5e16e97ba84e0e58b9ec
朱鴻埕 <marc.zhu@ehsn.com.tw>
2023-10-11 17:51:50 +0800

    complete table

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,19 +1,100 @@
 import Table from "./Table";
+import { useState } from "react";
+import { BiUpArrow, BiDownArrow } from "react-icons/bi";
 
 const SortableTable = (props) => {
-  const { config } = props;
+    const [sortOrder, setSortOrder] = useState(null);
+    const [sortBy, setSortBy] = useState(null);
+    const { config, data } = props;
 
-  const updateConfig = config.map((column) => {
-    if (!column.sortValue) {
-      return column;
+    const handleClick = (label) => {
+        // 點擊時變更state: sortOrder
+
+        if (label !== sortBy) {
+            setSortOrder("asc");
+        } else if (sortOrder === null) {
+            setSortOrder("asc");
+        } else if (sortOrder === "asc") {
+            setSortOrder("desc");
+        } else if (sortOrder === "desc") {
+            setSortOrder(null);
+        }
+        setSortBy(label);
+    };
+
+    const updateConfig = config.map((column) => {
+        if (!column.sortValue) {
+            return column;
+        }
+
+        return {
+            ...column,
+            header: () => (
+                <th
+                    className="cursor-pointer hover:bg-gray-100"
+                    onClick={() => {
+                        handleClick(column.label);
+                    }}
+                >
+                    <div className="flex items-center">
+                        {getIcons(column.label, sortBy, sortOrder)}
+                        {column.label}
+                    </div>
+                </th>
+            ),
+        };
+    });
+
+    let sortData = data; // default : 原始array
+    if (sortBy && sortOrder) {
+        // find sortValue function to use
+        const { sortValue } = config.find((column) => column.label === sortBy); // 僅回傳符合的第一個 element
+        sortData = [...data].sort((a, b) => {
+            const valueA = sortValue(a);
+            const valueB = sortValue(b);
+
+            const reversedOrder = sortOrder === "asc" ? 1 : -1;
+            if (typeof valueA === "string") {
+                return valueA.localeCompare(valueB) * reversedOrder;
+            }
+            return (valueA - valueB) * reversedOrder;
+        });
     }
 
-    return {
-      ...column,
-      header: () => <th>{column.label} is sortable</th>,
-    };
-  });
-  return <Table {...props} config={updateConfig} />; // 後面的config 覆蓋前面props的config
+    return <Table {...props} data={sortData} config={updateConfig} />; //data覆蓋props當中的data
 };
+
+// label跟sortBy是否相同，不同則呈現上下符號
+function getIcons(label, sortBy, sortOrder) {
+    if (label !== sortBy) {
+        return (
+            <div>
+                <BiUpArrow />
+                <BiDownArrow />
+            </div>
+        );
+    }
+
+    if (sortOrder === null) {
+        return (
+            <div>
+                <BiUpArrow />
+                <BiDownArrow />
+            </div>
+        );
+    } else if (sortOrder === "asc") {
+        return (
+            <div>
+                <BiUpArrow />
+            </div>
+        );
+    } else if (sortOrder === "desc") {
+        return (
+            <div>
+                <BiDownArrow />
+            </div>
+        );
+    }
+}
 
 export default SortableTable;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,36 +1,36 @@
 import { Fragment } from "react";
 function Table({ data, config, keyFn }) {
-  const renderHeaders = config.map((column) => {
-    if (column.header) {
-      return <Fragment key={column.label}>{column.header()}</Fragment>;
-    }
-    return <th key={column.label}>{column.label}</th>;
-  });
-
-  const renderRows = data.map((rowData) => {
-    const renderCells = config.map((column) => {
-      return (
-        <td className="p-2" key={column.label}>
-          {column.render(rowData)}
-        </td>
-      );
+    const renderHeaders = config.map((column) => {
+        if (column.header) {
+            return <Fragment key={column.label}>{column.header()}</Fragment>;
+        }
+        return <th key={column.label}>{column.label}</th>;
     });
 
-    return (
-      <tr className="border-b" key={keyFn(rowData)}>
-        {renderCells}
-      </tr>
-    );
-  });
-  return (
-    <table className="table-auto border-spacing-2">
-      <thead>
-        <tr className="border-b-2">{renderHeaders}</tr>
-      </thead>
+    const renderRows = data.map((rowData) => {
+        const renderCells = config.map((column) => {
+            return (
+                <td className="p-2" key={column.label}>
+                    {column.render(rowData)}
+                </td>
+            );
+        });
 
-      <tbody>{renderRows}</tbody>
-    </table>
-  );
+        return (
+            <tr className="border-b" key={keyFn(rowData)}>
+                {renderCells}
+            </tr>
+        );
+    });
+    return (
+        <table className="table-auto border-spacing-2">
+            <thead>
+                <tr className="border-b-2">{renderHeaders}</tr>
+            </thead>
+
+            <tbody>{renderRows}</tbody>
+        </table>
+    );
 }
 
 export default Table;
