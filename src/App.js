14a345b4350e3b9a83facf0d4cf51316538b85c7
aws753951 <a0983204766@gmail.com>
2023-10-15 14:09:12 +0800

    basic setting for redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import MoviePlaylist from "./components/MoviePlaylist";
+import SongPlaylist from "./components/SongPlaylist";
+import "./index.css";
 
 const App = () => {
-  return <div>App</div>;
+  const handleResetClick = () => {
+    //
+  };
+  return (
+    <div className="container is-fluid">
+      <button onClick={() => handleResetClick()} className="button is-danger">
+        Reset Both Playlists
+      </button>
+      <hr />
+      <MoviePlaylist />
+      <hr />
+      <SongPlaylist />
+    </div>
+  );
 };
 
 export default App;
