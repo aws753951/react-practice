5d3263dca357ca04981cbb52300e6962ee7d20da
aws753951 <a0983204766@gmail.com>
2023-10-15 15:00:34 +0800

    useSelector with state rendering

--- src/components/SongPlaylist.js
+++ src/components/SongPlaylist.js
@@ -1,12 +1,14 @@
 import { createRandomSong } from "../data";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addSong } from "../store";
 
 function SongPlaylist() {
   const dispatch = useDispatch();
   // To Do:
   // Get list of songs
-  const songPlaylist = [];
+  const songPlaylist = useSelector((state) => {
+    return state.songs;
+  });
 
   const handleSongAdd = (song) => {
     const action = addSong(song);
