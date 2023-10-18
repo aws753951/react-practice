074b5fdb60fd2d73ec3f98839ceec471708abc63
aws753951 <a0983204766@gmail.com>
2023-10-18 22:52:57 +0800

    circular import with function approach

--- src/App.js
+++ src/App.js
@@ -6,24 +6,21 @@ import { useDispatch } from "react-redux";
 import { reset } from "./store";
 
 const App = () => {
-    const dispatch = useDispatch();
-    const handleResetClick = () => {
-        dispatch(reset());
-    };
-    return (
-        <div className="container is-fluid">
-            <button
-                onClick={() => handleResetClick()}
-                className="button is-danger"
-            >
-                Reset Both Playlists
-            </button>
-            <hr />
-            <MoviePlaylist />
-            <hr />
-            <SongPlaylist />
-        </div>
-    );
+  const dispatch = useDispatch();
+  const handleResetClick = () => {
+    dispatch(reset());
+  };
+  return (
+    <div className="container is-fluid">
+      <button onClick={() => handleResetClick()} className="button is-danger">
+        Reset Both Playlists
+      </button>
+      <hr />
+      <MoviePlaylist />
+      <hr />
+      <SongPlaylist />
+    </div>
+  );
 };
 
 export default App;
