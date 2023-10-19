1f68364dc40afe919a64825d819cba311bdb14e4
朱鴻埕 <marc.zhu@ehsn.com.tw>
2023-10-19 15:23:24 +0800

    get onchange value for showing css

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -4,21 +4,35 @@ import { removeCar } from "../store";
 const CarList = () => {
     const dispath = useDispatch();
 
-    const cars = useSelector(({ cars: { searchTerm, data } }) => {
-        return data.filter((car) => {
-            return car.name.toLowerCase().includes(searchTerm.toLowerCase());
-        });
-    });
+    // 取得form的name，為onChange，所以取得其name不是在submit後取得的
+    const { name, cars } = useSelector(
+        ({ form: { name }, cars: { searchTerm, data } }) => {
+            const filterCars = data.filter((car) => {
+                return car.name
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase());
+            });
+
+            return {
+                cars: filterCars,
+                name,
+            };
+        }
+    );
 
     const handleDeleteCar = (car) => {
         dispath(removeCar(car.id));
     };
     const renderCars = cars.map((car) => {
+        const bold =
+            name && car.name.toLowerCase().includes(name.toLowerCase());
+
         return (
-            <div key={car.id} className="panel">
+            <div key={car.id} className={`panel ${bold && "bold"}`}>
                 <p>
                     {car.name} - ${car.cost}
                 </p>
+
                 <button
                     className="button is-danger"
                     onClick={() => {
