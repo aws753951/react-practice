b1e2fbcde729f45d501d52a91e4a97e53be7db64
aws753951 <a0983204766@gmail.com>
2023-10-22 22:03:58 +0800

    dev for setTimeout

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -3,7 +3,16 @@ import axios from "axios";
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
+
+  await pause(2000);
+
   return response.data;
 });
+
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 export { fetchUsers };
