3ca74d313911392460bc8da0073a553d839ac8d7
朱鴻埕 <marc.zhu@ehsn.com.tw>
2023-10-24 10:15:48 +0800

    新增用戶資料thunks

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,36 +1,50 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "../store";
+import { fetchUsers, addUser } from "../store";
 import Skeleton from "./Skeleton";
+import Button from "./Button";
 
 const UsersList = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(fetchUsers());
-  }, [dispatch]);
+    const { isLoading, data, error } = useSelector((state) => state.users);
 
-  const { isLoading, data, error } = useSelector((state) => state.users);
+    useEffect(() => {
+        console.log(dispatch(fetchUsers()))
+    }, [dispatch]);
 
-  if (isLoading) {
-    return <Skeleton times={6} className={"h-10 w-full"} />;
-  }
+    const handleUserAdd = () => {
+        dispatch(addUser());
+    };
 
-  if (error) {
-    return <div>Error fetching data...</div>;
-  }
+    if (isLoading) {
+        return <Skeleton times={6} className={"h-10 w-full"} />;
+    }
 
-  const renderdUsers = data.map((user) => {
+    if (error) {
+        console.log(error);
+        return <div>Error fetching data...</div>;
+    }
+
+    const renderdUsers = data.map((user) => {
+        return (
+            <div key={user.id} className="mb-2 border rounded">
+                <div className="flex p-2 justify-between items-center cursor-pointer">
+                    {user.name}
+                </div>
+            </div>
+        );
+    });
+
     return (
-      <div key={user.id} className="mb-2 border rounded">
-        <div className="flex p-2 justify-between items-center cursor-pointer">
-          {user.name}
+        <div>
+            <div className="flex justify-between items-center">
+                <h1 className="m-2 text-xl">Users</h1>
+                <Button onClick={handleUserAdd}>+ Add User</Button>
+            </div>
+            {renderdUsers}
         </div>
-      </div>
     );
-  });
-
-  return <div>{renderdUsers}</div>;
 };
 
 export default UsersList;
