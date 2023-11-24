da5fc2ead69c8db02250e58b696edc4785c83f53
朱鴻埕 <marc.zhu@ehsn.com.tw>
2023-11-24 15:11:49 +0800

    useSelector with 2 variable and change css with them

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -2,19 +2,28 @@ import { useSelector, useDispatch } from "react-redux";
 import { removeCar } from "../store";
 const CarList = () => {
     const dispatch = useDispatch();
-    const cars = useSelector(({ cars: { searchTerm, data } }) => {
-        return data.filter(
-            (car) => car.name.toLowerCase().includes(searchTerm.toLowerCase()) // includes("") 則都會包含
-        );
-    });
+    const { name, cars } = useSelector(
+        ({ form: { name }, cars: { searchTerm, data } }) => {
+            return {
+                name,
+                cars: data.filter(
+                    (car) =>
+                        car.name
+                            .toLowerCase()
+                            .includes(searchTerm.toLowerCase()) // includes("") 則都會包含
+                ),
+            };
+        }
+    );
 
     // 搭配 renderCar，每次iterate時投入car
     const handleDeleteCar = (car) => {
         dispatch(removeCar(car.id));
     };
     const renderCars = cars.map((car) => {
+        const red = name && car.name.toLowerCase().includes(name.toLowerCase());
         return (
-            <div className="flex" key={car.id}>
+            <div className={`flex ${red && "text-red-200"}`} key={car.id}>
                 <p>
                     {car.name} - ${car.cost}
                 </p>
